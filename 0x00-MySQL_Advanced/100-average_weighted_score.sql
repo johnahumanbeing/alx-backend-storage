d672d300330c5a6cf4f5f7d14bec49d11c4982b9
johnahumanbeing <mseewak@gmail.com>
2024-05-09 22:09:58 +0300

    SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,11 +1,12 @@
--- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
--- that computes and store the average weighted score for a student.
+-- Creates stored procedure ComputeAverageWeightedScoreForUser
+-- that computes and stores the average weighted score for a
+-- student
 DELIMITER $
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average_weight_score, total_projects_weghted_score FLOAT;
+    DECLARE average_weight_score, total_projects_weighted_score FLOAT;
     DECLARE total_projects_weight INT;
-    SELECT SUM(projects.score * projects.weight) INTO total_projects_weghted_score
+    SELECT SUM(score * projects.weight) INTO total_projects_weighted_score
         FROM corrections JOIN projects ON project_id = projects.id
         WHERE corrections.user_id = user_id GROUP BY corrections.user_id;
     SELECT SUM(projects.weight) INTO total_projects_weight FROM projects;
