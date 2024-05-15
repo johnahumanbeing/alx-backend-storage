aad040bfcd52491a6ead99cf5cafe7131ac2eacc
johnahumanbeing <mseewak@gmail.com>
2024-05-15 20:08:12 +0300

    redis client module (fix1)

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,12 +10,27 @@ from typing import Any, Callable, Optional, Union
 def count_calls(method: Callable) -> Callable:
     """ Count the number of times a method is called
     """
-    key = method.__qualname__
-
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """ Wrapper function
         """
-        self._redis.incr(key)
+        self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ Store the history of inputs and outputs for a method
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function
+        """
+        input = str(args)
+        self._redis.rpush(f"{method.__qualname__}:inputs", input)
+
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(f"{method.__qualname__}:outputs", output)
+
+        return output
     return wrapper
