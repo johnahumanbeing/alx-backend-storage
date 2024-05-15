7caac90ba42e05641c7fc5d011ddb5bf5d1abf99
johnahumanbeing <mseewak@gmail.com>
2024-05-15 20:27:37 +0300

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" A caching request module
+"""
+import redis
+import requests
+from fuctools import Callable
+
+
+def track_get_page(fn: Callable) -> Callable:
+    """ gettor for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """
+        wrapper that checks if a url's data is cached
+        and tracks how many times the get_page is called.
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
+
+
+@track_get_page
+def get_page(url: str) -> str:
+    """
+    Makes a http request to a given endpoint
+    params:
+        url - the url of the web page to retreive
+    Returns:
+        The content of the web page.
+    """
+    response = requests.get(url)
+    return response.text
